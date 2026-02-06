65d873e700dc01c6167fd0e839cf0ae94165c0f7
Camilo Andrés Medina Rivera <medina1camilo@gmail.com>
2026-02-06 11:53:37 +0100

    [main] baseline

---
+++ src/main/java/co/medina/camilo/rivera/mediweedback/MediweedBackApplication.java
@@ -0,0 +1,13 @@
+package co.medina.camilo.rivera.mediweedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediweedBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediweedBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/medina/camilo/rivera/mediweedback/MediweedBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.medina.camilo.rivera.mediweedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediweedBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
