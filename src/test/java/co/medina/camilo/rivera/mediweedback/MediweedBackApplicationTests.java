b6deef7bd528b105de92bab95469ff9445892b77
Camilo Andrés Medina Rivera <medina1camilo@gmail.com>
2026-02-09 13:12:15 +0100

    [feature/build-enchanment] fix tests

--- src/test/java/co/medina/camilo/rivera/mediweedback/MediweedBackApplicationTests.java
+++ src/test/java/co/medina/camilo/rivera/mediweedback/MediweedBackApplicationTests.java
@@ -1,13 +1,28 @@
 package co.medina.camilo.rivera.mediweedback;
 
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
 
+@RequiredArgsConstructor
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @SpringBootTest
 class MediweedBackApplicationTests {
 
+    private final MediweedBackApplication mediweedBackApplication;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(mediweedBackApplication);
     }
 
 }
